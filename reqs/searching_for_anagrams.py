c612fa7ef68b9d760e737860bf1ffc1082daaee2
jamolanguekd <jamolanguekd@gmail.com>
2018-10-08 23:54:26 +0800

    - finished seeding_words - finished picking_words

--- reqs/searching_for_anagrams.py
+++ reqs/searching_for_anagrams.py
@@ -13,14 +13,26 @@ def word_picker(list_dictionary, str_position):
         word_list.append(list_dictionary[int(position)])
     return word_list
 
+def anagram_searcher(str_word_input, list_dictionary):
+    sorted_word = ''.join(sorted(str_word_input))
+    anagram_list = []
+    for word in list_dictionary:
+        if ''.join(sorted(word)) == sorted_word:
+            anagram_list.append(word)
+    if str_word_input not in anagram_list:
+        anagram_list.append(str_word_input)
+    anagram_list.sort()
+    return anagram_list
+
 
 filename = str(input())
 dictionary = load_dictionary(filename)
 test_cases = int(input())
 
 for i in range(test_cases):
-    word_pos = str(input())
-    new_word_list = word_picker(dictionary, word_pos)
-    for word in new_word_list:
-        print(word, end=' ')
+    word_input = str(input())
+    matching_words = anagram_searcher(word_input, dictionary)
+
+    for item in matching_words:
+        print(item, end=' ')
     print()
